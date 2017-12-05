150c4e809079cb22515f7b4b179f167772aecdee
spenceralpaugh <32199847+spenceralpaugh@users.noreply.github.com>
2017-12-05 12:31:31 -0500

    Update Visualization.R

--- Visualization.R
+++ Visualization.R
@@ -1,10 +1,12 @@
 load('synthetic_ESSCLC_data.RData')
 
-IDS = sample(unique(dat$ID),500)
+foo = matrix(0,nrow=nrow(episodes),ncol = length(t))
+dat = data.frame(episodes[,c('ID','setting')],foo)
+IDS = sample(unique(dat$ID),500) #this does not work with the current changes to the code, dat is not found and I can't remember where it was supposed to come from
 episodes = episodes[episodes$ID %in% IDS,]
 summary.stats = summary.stats[summary.stats$ID %in% IDS,]
 
-#IDS <- sample(unique(episodes$ID),500)
+#IDS <- sample(unique(episodes$ID),500)    #took from Initial Investigations 
 #episodes <- episodes[episodes$ID %in% IDS,]
 #summary.stats <- summary.stats[summary.stats$ID %in% IDS,]
 
@@ -74,7 +76,7 @@ snap5 = reshape(snap4,direction='wide',idvar='ID',timevar = 'setting')
 
 
 require(ade4)
-D1 = dist.binary(foo4[,-1],method=2)
+D1 = dist.binary(foo4[,-1],method=2) #needs fixing for new code
 
 D2 = dist(summary.stats[summary.stats$ID %in% IDS,c(2:5)])
 D2 = D2/max(D2)
@@ -97,30 +99,28 @@ Viz = function(D,widedat,stats,DLabels){
       sidebarLayout(
         sidebarPanel(
           uiOutput('weights')),
-        mainPanel(plotOutput("Viz",height='2000px'))
+        mainPanel(plotOutput("Viz",height='500px'))
       )
     ),
     server = function(input, output){
       output$weights = renderUI({
         lapply(1:(Dm), function(i) {
-          sliderInput(paste0('w', i), DLabels,
+          sliderInput(paste0('w', i), DLabels[i],
                       value = 1/Dm,0,1)
         })})
       #cluster based on new distance
-      
-      
-      
-      
-      
+
       
       output$Viz = renderPlot({
-        Dtmp = Reduce('+',lapply(1:(Dm), function(i)
-          eval(parse(text=paste0('input$w',i,'*D[[',i,']]')))))
-        K = 5
+        w=c(.1,.1,.2,.3,.3)
+        Dtmp = Reduce('+',lapply(1:(Dm), function(i) eval(parse(text=paste0('input$w',i,'*D[[',i,']]')))))
+          #w[i]*D[[i]]))
+       
+        K = 5 #119-121
         clust = pam(Dtmp,k=K)
         P = prop.table(table(clust$clustering))
         #clust$silinfo$avg.width
-        par(mfrow=c(K,3))
+        par(mfrow=c(1,K))
         for(k in 1:K){
           foo = colMeans(widedat[clust$clustering ==k,-1])
           plot(foo[1:99],type='n',main=paste0('Cluster ',k,':',round(P[k],2)),ylab='Proportion',xlab='',xaxt='n',ylim=c(0,1))
@@ -128,8 +128,8 @@ Viz = function(D,widedat,stats,DLabels){
             lines(foo[(1+99*(j-1)):(99 + 99*(j-1))],col=j)
           }
           mtext(paste0('Mean Survival: ',round(colMeans(stats[clust$clustering ==k,9],na.rm=TRUE),2),' Days'),1) #could be median... try using apply()
-          boxplot(stats[clust$clustering ==k,1:4],ylim=c(0,1))
-          boxplot(stats[clust$clustering ==k,5:8],ylim=c(0,20))
+          #boxplot(stats[clust$clustering ==k,1:4],ylim=c(0,1))
+          #boxplot(stats[clust$clustering ==k,5:8],ylim=c(0,20))
           #abline(v=seq(.25,.8,by=.25),col='black',lwd=1)
           #axis(1,at=seq(0,.98,by=0.0625),label=rep(c(0,0.25,0.5,0.75),4))
           #mtext(c('None','Hospital','Hospice','SNF'),3,line=.75,at=seq(.1,.95,by=.25),cex=.8)
@@ -141,13 +141,14 @@ Viz = function(D,widedat,stats,DLabels){
 }
 
 
-Viz(list(D1,D2,D3,D4,D5),widedat=foo4,stats=summary.stats[c(2:9,13)])
+Viz(list(D1,D2,D3,D4,D5),widedat=foo4,stats=summary.stats[c(2:9,13)],DLabels = paste0('D',1:5))
 
 image(as.matrix(D1-D2),breaks=seq(-1,1,length=13))  #Why are these so similar?
 image(as.matrix(D1-D3),breaks=seq(-1,1,length=13)) 
 image(as.matrix(D1-D4),breaks=seq(-1,1,length=13)) 
 image(as.matrix(D1-D5),breaks=seq(-1,1,length=13)) 
 image(as.matrix(D4-D5),breaks=seq(-1,1,length=13)) 
+
 
 
 
